5f27be416984055c3b48a0dc0a64128b3e62e21a
Oleksandr Chyhryn <as.chigrin@gmail.com>
2022-09-06 02:12:51 +0300

    Додає виконану домашню роботу до репозиторія

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onCurrentTime, 1000));
+
+function onCurrentTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(savedTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+const inputData = {};
+
+checkLocalStorage();
+
+formEl.addEventListener('input', throttle(onInputData, 500));
+formEl.addEventListener('submit', onSubmitForm);
+
+function onInputData(event) {
+  inputData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
+  console.log('saveData', localStorage.getItem(STORAGE_KEY));
+}
+
+function checkLocalStorage() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (localStorage.getItem(STORAGE_KEY) !== null) {
+    inputEl.value = savedData.email;
+    textareaEl.value = savedData.message;
+  } else {
+    inputEl.value = '';
+    textareaEl.value = '';
+  }
+}
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  inputData[inputEl.name] = inputEl.value;
+  inputData[textareaEl.name] = textareaEl.value;
+  console.log(inputData);
+  localStorage.removeItem(STORAGE_KEY);
+  checkLocalStorage();
+}
