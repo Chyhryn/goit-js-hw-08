a33a7d718b622344d5e5c9c358d4606e17308ae2
Oleksandr Chyhryn <as.chigrin@gmail.com>
2022-09-24 19:59:05 +0300

    Починає все спочатку від додавання файлів до репозиторію

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,5 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-const gallery = document.querySelector('.gallery');
-
-const galleryMarkup = galleryItems
-  .map(
-    item => `<a class="gallery__item" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      alt="${item.description}"
-    />
-  </a>`
-  )
-  .join('');
-
-gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-
-new SimpleLightbox('.gallery__item', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
 
 console.log(galleryItems);
